7e1e699263c0761782bdbd442c3753843b5e6d6b
Nicholas003 <39424835+Nicholas003@users.noreply.github.com>
2025-03-21 01:37:15 +0000

    Deploying to gh-pages from @ Nicholas003/vhAstro-Theme@e089ebed36d8d75a7aacee42be03433284c22da3 🚀

--- _worker.js/_astro-internal_middleware.mjs
+++
@@ -1,1 +0,0 @@
-globalThis.process??={},globalThis.process.env??={};import"./chunks/astro-designed-error-pages_DZKQpR4q.mjs";import"./chunks/astro/server_DwlEfa97.mjs";import{s as sequence}from"./chunks/index_DpuzOy4_.mjs";const onRequest$1=(e,s)=>(e.isPrerendered&&(e.locals.runtime??={env:process.env}),s()),onRequest=sequence(onRequest$1);export{onRequest};
--- _worker.js/chunks/_noop-actions_Dk2yPKP3.mjs
+++
@@ -1,1 +0,0 @@
-globalThis.process??={},globalThis.process.env??={};const server={},actions=Object.freeze(Object.defineProperty({__proto__:null,server:server},Symbol.toStringTag,{value:"Module"}));export{actions as a,server as s};
